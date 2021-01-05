0710bb5d389865c4bc2dd349c801f8b748bc2d80
pedro javier <pedro.javier.valeriano.99@gmail.com>
2021-01-05 17:33:51 -0500

    Updates

--- src/enlaces.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Enlaces = ({title}) => (
-      <li className="nav-item ">
-        <a className="nav-link" href="#">{title} <span className="sr-only">(current)</span></a>
-      </li>
-)
-export default Enlaces
--- src/fotos.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
- 
- const Fotos =({image,title}) => (
-      <div className="carousel-item ">
-         <img src={image} className="d-block w-100" alt={title}/>
-       </div>
- )
- export default Fotos
